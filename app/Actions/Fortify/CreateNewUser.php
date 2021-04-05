ed5666f438728fee2b97f628626357cce1d6ea61
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2021-04-05 15:49:19 -0500

    create user modified

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -21,20 +21,20 @@ class CreateNewUser implements CreatesNewUsers
     public function create(array $input)
     {
         Validator::make($input, [
-            'firstname' => ['required', 'string', 'max:255'],
+            'first_name' => ['required', 'string', 'max:255'],
             //'secondname' => ['string', 'max:255'],
-            'lastname' => ['required', 'string', 'max:255'],
-            'motherslastname' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'second_last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
         ])->validate();
 
         return User::create([
-            'firstname' => $input['firstname'],
-            'secondname' => $input['secondname'],
-            'lastname' => $input['lastname'],
-            'motherslastname' => $input['motherslastname'],
+            'first_name' => $input['first_name'],
+            'second_name' => $input['second_name'],
+            'last_name' => $input['last_name'],
+            'second_last_name' => $input['second_last_name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
