74c4aafa1fa19f50db3ea0a985b2c597467b8f6d
Pedro Adrian Sanchez Cardenas <pedrosanchezcardenas@gmail.com>
2021-04-20 20:12:38 -0500

    Shoping Cart

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -6,13 +6,16 @@ use App\Models\InventoryProduct;
 use Illuminate\Http\Request;
 
 use App\Models\Product;
+use App\Models\ShopingCart;
+
 class FrontController extends Controller
 {
     public function index(){
 
         $productos    =   Product::all();
         $price = InventoryProduct::orderBy('sale_price', 'desc')->get();
-        return view('welcome',compact('productos','price'));
+        $shopingItems = ShopingCart::where('user_id', auth()->user()->id)->get();
+        return view('welcome',compact('productos','price', 'shopingItems'));
 
     }
     public function show($id){
