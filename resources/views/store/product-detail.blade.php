fbee12b5f3e4459d0da719b1922c1f62b81f09a0
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2021-04-26 01:06:19 -0500

    delete file wrong

--- resources/views/store/product-detail.blade.php
+++ resources/views/store/product-detail.blade.php
@@ -65,7 +65,7 @@
                             <div class="w-full md:w-full px-3 mb-2 mt-2">
                                 <textarea class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white" name="comment" id="comment" placeholder='Escribe tu comentario' required></textarea>
                             </div>
-                            <div class="w-full md:w-full flex items-start md:w-full px-3">
+                            <div class="w-full flex items-start md:w-full px-3">
                                 <div class="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
                                 <svg fill="none" class="w-5 h-5 text-gray-600 mr-1" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
