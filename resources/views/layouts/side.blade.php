c07a24fbad39d4028fdd8c1373f70cbee0e50f0e
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2021-04-28 01:28:45 -0500

    update

--- resources/views/layouts/side.blade.php
+++ resources/views/layouts/side.blade.php
@@ -5,7 +5,7 @@
             <!-- Dashboards links -->
             <div class="flex-shrink-0 px-2 py-4 space-y-2">
                 <a href="{{ route('welcome') }}" type="button"
-                    class="flex items-center justify-center w-full px-4 py-2 text-sm text-white rounded-md bg-primary hover:bg-primary-dark focus:outline-none focus:ring focus:ring-primary-dark focus:ring-offset-1 focus:ring-offset-white dark:focus:ring-offset-dark">
+                    class="">
 
                     <span>Tienda</span>
                 </a>
