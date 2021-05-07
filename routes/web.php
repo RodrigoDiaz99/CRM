7dde3b1255d9dd32b738e5d0138321be7b173d81
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2021-05-06 19:42:44 -0500

    roles and auth

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
     return view('dashboard');
 })->name('dashboard');*/
 
-Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+
 
 
 $content1 = bannerone::orderBy('id', 'desc')->get()->take(1);
@@ -32,49 +32,52 @@ $content2 = bannertwo::orderBy('id', 'desc')->get()->take(1);
 $content3 = bannerthree::orderBy('id', 'desc')->get()->take(1);
 
 Route::get('/', 'FrontController@index',compact('content1','content2','content3'))->name('welcome');
-
-
+Route::get('product/addShopingCart/{id}', 'FrontController@addShopingCart')->name('addShopingCart');
+Route::post('contact/store', 'FrontController@sendEmail')->name('contact_send');
 Route::get('shop', 'FrontController@shop')->name('shop');
 Route::get('contact', 'FrontController@contact')->name('contact');
 Route::post('comments', 'FrontController@storeComment')->name('storeComment');
 Route::get('product/info/{id}', 'FrontController@show')->name('details.show');
-Route::post('checkout/confirm', 'FrontController@confirm')->name('confirm');
-Route::post('contact/store', 'FrontController@sendEmail')->name('contact_send');
 Route::post('contact', 'FrontController@contact')->name('contact');
-Route::get('product/addShopingCart/{id}', 'FrontController@addShopingCart')->name('addShopingCart');
-Route::get('checkout', 'FrontController@checkout')->name('checkout');
-Route::get('checkout/payment', 'FrontController@payment')->name('payment');
-Route::get('comments/list', 'commentsController@index')->name('comments.list');
-/*Aqui empiezan las rutas de los banners*/
-Route::get('index/elements', 'ContentController@index')->name('content.list');
-Route::get('first/create', 'ContentController@create1')->name('create_one');
-Route::post('first/store', 'ContentController@storeone')->name('store_one');
-Route::post('first/edit/{id}', 'ContentController@edit1')->name('edit_one');
-Route::post('first/update', 'ContentController@update1')->name('update_one');
 
-Route::get('second/create', 'ContentController@create2')->name('create_second');
-Route::post('second/store', 'ContentController@store2')->name('store_second');
-Route::post('first/store', 'ContentController@storeone')->name('store_one');
-Route::post('first/store', 'ContentController@storeone')->name('store_one');
+Route::group(['middleware' => ['auth','verified']], function () {
+    Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+    Route::post('checkout/confirm', 'FrontController@confirm')->name('confirm');
+    Route::get('checkout', 'FrontController@checkout')->name('checkout');
+    Route::get('checkout/payment', 'FrontController@payment')->name('payment');
+    Route::get('comments/list', 'commentsController@index')->name('comments.list');
+    /*Aqui empiezan las rutas de los banners*/
+    Route::get('index/elements', 'ContentController@index')->name('content.list');
+    Route::get('first/create', 'ContentController@create1')->name('create_one');
+    Route::post('first/store', 'ContentController@storeone')->name('store_one');
+    Route::get('first/edit/{id}', 'ContentController@edit1')->name('edit_one');
+    Route::put('first/update/{id}', 'ContentController@update1')->name('update_one');
+    Route::get('second/create', 'ContentController@create2')->name('create_second');
+    Route::post('second/store', 'ContentController@store2')->name('store_second');
+    Route::post('first/store', 'ContentController@storeone')->name('store_one');
+    Route::post('first/store', 'ContentController@storeone')->name('store_one');
+    Route::get('third/create', 'ContentController@create3')->name('create_three');
+    Route::post('third/store', 'ContentController@store3')->name('store_three');
+    Route::post('first/store', 'ContentController@storeone')->name('store_one');
+    Route::post('first/store', 'ContentController@storeone')->name('store_one');
+    Route::get('reports/sales', 'ReportController@index2')->name('sales.index');
+    Route::get('reports/sales', 'ReportController@index2')->name('sales.index');
+    /*Aqui termina la ruta de los banners*/
+    Route::resource('products/category', CategoryProductController::class);
+    Route::resource('products', ProductController::class);
+    Route::resource('promotions', PromotionController::class);
+    Route::resource('inventory', InventoryProductController::class);
+    Route::resource('delivery', DeliveryDataController::class);
+    //Route::resource('report/sales', ReportController::class);
+    //Reports Resource
+    Route::resource('report', ReportController::class);
+    //Voucher resource
+    Route::resource('voucher', VoucherController::class);
+    //Cash Fund Resource
+    Route::resource('cashfund', CashFundController::class);
+    
+});
 
-Route::get('third/create', 'ContentController@create3')->name('create_three');
-Route::post('third/store', 'ContentController@store3')->name('store_three');
-Route::post('first/store', 'ContentController@storeone')->name('store_one');
-Route::post('first/store', 'ContentController@storeone')->name('store_one');
-/*Aqui termina la ruta de los banners*/
-Route::resource('products/category', CategoryProductController::class);
-Route::resource('products', ProductController::class);
-Route::resource('promotions', PromotionController::class);
-Route::resource('inventory', InventoryProductController::class);
-Route::resource('delivery', DeliveryDataController::class);
-Route::get('reports/sales', 'ReportController@index2')->name('sales.index');
-//Route::resource('report/sales', ReportController::class);
-//Reports Resource
-Route::resource('report', ReportController::class);
-//Voucher resource
-Route::resource('voucher', VoucherController::class);
-//Cash Fund Resource
-Route::resource('cashfund', CashFundController::class);
 
 
 
