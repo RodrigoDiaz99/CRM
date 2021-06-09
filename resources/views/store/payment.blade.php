2666d90ab0beed25afe6a1b35d1d08ce17a2ade8
KennAyala <kennayala@outlook.com>
2021-06-09 12:51:48 -0500

    ª

--- resources/views/store/payment.blade.php
+++ resources/views/store/payment.blade.php
@@ -201,13 +201,15 @@
                 <div class="flex justify-center lg:justify-end">
                     <div class="border rounded-md max-w-md w-full px-4 py-3">
                         <div class="flex items-center justify-between">
-                            <h3 class="text-gray-700 font-medium">Order total (1)</h3>
+                            <h3 class="text-gray-700 font-medium">Total de orden ({{count($ShoppingCart)}})</h3>
+                            <span class="text-gray-600 text-sm">Edit</span>
                         </div>
                         <div class="flex justify-between mt-6">
                             <div class="flex">
                                 <img class="h-20 w-20 object-cover rounded"
-                                    src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"
-                                    alt="">
+                                src="{{ Storage::url($item->products->img_paths) }}" alt="{{ Storage::url($item->products->name) }}>
+                                                        <img class="h-20 w-20 object-cover rounded" src="{{ Storage::url($item->products->img_paths) }}" alt="{{ Storage::url($item->products->name) }}">
+
                                 <div class="mx-3">
                                     <h3 class="text-sm text-gray-600">{{ $item->products->name }}</h3>
                                     <div class="flex items-center mt-2">
@@ -228,12 +230,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <span class="text-gray-600">$@php
-                                $price = $item->products->inventories['0']->sale_price;
-                                $total = $total + $price;
-                                echo $price
-                            @endphp
-                        </span>
+                            <span class="text-gray-600">${{ $item->products->inventories['0']->sale_price }}</span>
                         </div>
                     </div>
                 </div>
