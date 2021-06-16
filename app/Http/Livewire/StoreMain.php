07c39b7faad4dd1b779e22cd8f723d3d715dc14f
Pedro Adrián Sánchez Cárdenas <pedrosanchezcardenas@gmail.com>
2021-06-16 13:40:02 -0500

    Promotions update

--- app/Http/Livewire/StoreMain.php
+++ app/Http/Livewire/StoreMain.php
@@ -40,18 +40,18 @@ class StoreMain extends Component
 
                 $expiration = explode('-', $promotion['expiration_date']);
 
+                $producId = ShoppingCartStore::latest('id')->first();
+
                 if(($expiration[2] <= $dia) && ($expiration[1] <= $mes) && ($año[0] <= $año)){
-                    $discount = ($sale_price*$cash_discount)/100;
+                    $discount = ($sale_price * $cash_discount) / 100;
                     $sale_price = $sale_price - $discount;
 
-                    ShoppingCart::where('id', $id->id)->update([
-                        'price' => $price->sale_price
-                        ]); 
-                }else{
-                    ShoppingCart::where('id', $id->id)->update([
+                    ShoppingCart::where('id', $producId->id)->update([
                         'price' => $price->sale_price
                     ]);
 
+                }else{
+                   //NOTING
                 }
             }
 
